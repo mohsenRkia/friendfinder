661c6e57b37f3943add98a69b69a188ec7364559
MohsenRkia <mohsenrostamikia@gmail.com>
2018-12-05 16:15:41 +0330

    ContactUs

--- resources/views/v1/site/layouts/header.blade.php
+++ resources/views/v1/site/layouts/header.blade.php
@@ -3,12 +3,15 @@
         <div class="container">
             <input type="search">
             <hr>
-            hello
+            <a href="{{route('home.index')}}">Home</a>
             <hr>
-            hello
+            <a href="{{route('contact.index')}}">ContactUs</a>
             <hr>
-            hello
+            <a href="{{route('home.index')}}/login">Login</a>
             <hr>
+            @if(Auth::user())
+                <a href="{{route('user.profile.timeline',['id' => Auth::user()->id,'name' => Auth::user()->name])}}">My Profile</a>
+            @endif
 
         </div><!-- /.container -->
     </nav>
